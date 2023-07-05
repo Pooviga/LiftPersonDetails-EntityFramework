73826955d4a00b8d319ba2297e7eaf66fcd7f2f0
Github Pooviga <Github spooviga01@gmail.com>
2023-07-05 14:47:26 +0530

    added innermemory

--- PersonDetails.WebAPi/Program.cs
+++ PersonDetails.WebAPi/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<PersonDetailDBContext>(options => options.UseInMemoryDatabase("NafInMemory"));
+
 builder.Services.AddDbContext<PersonDetailDBContext>(options => options.UseSqlServer(
 
     builder.Configuration.GetConnectionString("NafTeam2ConnString"))
